83823363fa576695e6ac0c7fa840fe9b839099cc
parkercummins <119359843+parkercummins@users.noreply.github.com>
2024-10-04 11:12:33 -0400

    Updated Money.ts

--- src/client/modules/Money/Money.ts
+++ src/client/modules/Money/Money.ts
@@ -1,19 +1,30 @@
 import { UI } from "client/onload/UI/currency";
 import { BigNumber } from "shared/modules/BigNumber/BigNumber";
 
-
+// Money for the UI
 export namespace Money {
     const State = {
-        Money: new BigNumber(0),
+        Money: new BigNumber(0) ,
     }
 
     export const GetMoney = () => {
         return State.Money;
     }
 
+    //Increment
     export const AddMoney = (Value: BigNumber) => {
         const Final = State.Money.Add(Value);
         UI.Currency.TweenMoney(State.Money, Final);
         State.Money = Final;
     }
+<<<<<<< Updated upstream
+=======
+
+    //Decrement
+    export const RemoveMoney = (Value: BigNumber) => {
+        const Final = State.Money.Subtract(Value);
+        UI.Currency.TweenMoney(State.Money, Final);
+        State.Money = Final;
+    }
+>>>>>>> Stashed changes
 }
